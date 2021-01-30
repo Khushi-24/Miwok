345e3a8534e274562ff8fe7fdf762a5b6a9727ab
Khushi-24 <khushi.jiyani1@gmail.com>
2021-01-30 17:46:47 +0530

    Overrided onstop() method in order to release the memory when user switches to another activity.

--- app/src/main/java/com/example/miwok/Phrases.java
+++ app/src/main/java/com/example/miwok/Phrases.java
@@ -56,6 +56,13 @@ public class Phrases extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
     private void releaseMediaPlayer() {
         // If the media player is not null, then it may be currently playing a sound.
         if (mediaPlayer != null) {
